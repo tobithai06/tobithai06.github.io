c7c0582e6ba340fccdb7ccc257f49a6b4f9227af
Tobi Thai <tobi.thai06@gmail.com>
2025-11-16 23:59:15 -0500

    Add files via upload

---
+++ scripts/scripts1.js
@@ -0,0 +1,11 @@
+/*
+Student Name: Tobi Thai
+File Name: scripts1.js
+Date: 11/16/2025
+*/
+
+//Last Modified
+function displayLastModified() {
+    const lastModifiedDiv = document.getElementById("lastModified");
+    lastModifiedDiv.innerHTML = "Last Modified: " + document.lastModified;
+}
